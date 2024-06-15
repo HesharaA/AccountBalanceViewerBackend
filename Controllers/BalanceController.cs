eb37218c0839ecaa77fd5d26da38ecfaa437c9f3
Heshara@99x <151610769+HesharaA@users.noreply.github.com>
2024-06-15 14:19:26 +0530

    Refacotor file processing

--- Controllers/BalanceController.cs
+++ Controllers/BalanceController.cs
@@ -10,6 +10,12 @@ public class BalancesController : ControllerBase
 {
     private readonly IAccountBalanceRepository _repo;
 
+    enum FileType
+    {
+        tab,
+        xl,
+    }
+
     public BalancesController(IAccountBalanceRepository repo)
     {
         _repo = repo;
@@ -22,94 +28,123 @@ public class BalancesController : ControllerBase
         return balance != null;
     }
 
-    private async Task<List<AccountBalance>> ProcessTextFile(IFormFile file)
+    private async Task<List<AccountBalance>> ProcessFile(IFormFile file, FileType type)
     {
         List<AccountBalance> balances = [];
 
-        using (var stream = new StreamReader(file.OpenReadStream()))
+        switch (type)
         {
-            int lineCount = 0;
-            string? line = await stream.ReadLineAsync();
-            while (line != null)
-            {
-                var commaSeparated = line!.Split(',');
-                var columns = line!.Split('\t');
+            case FileType.tab:
+                {
 
-                if (lineCount == 6 && columns.Length == 1 && columns[0].IsNullOrEmpty())
-                {
-                    line = await stream.ReadLineAsync();
-                    continue;
-                }
-                else if (lineCount > 5 || columns.Length != 3 || commaSeparated.Length > columns.Length)
-                {
-                    throw new InvalidOperationException("The file is not a valid tab-separated file.");
-                }
-
-                if (DateTime.TryParse(columns[2], out var balanceDate))
-                {
-                    if (!await isAccountBalancePresent(balanceDate))
+                    using var stream = new StreamReader(file.OpenReadStream());
+                    int lineCount = 0;
+                    string? line = await stream.ReadLineAsync();
+                    while (line != null)
                     {
-                        var newAccountBalance = new AccountBalance
-                        {
-                            AccountName = columns[0],
-                            Balance = decimal.Parse(columns[1]),
-                            BalanceDate = balanceDate
-                        };
 
-                        balances.Add(newAccountBalance);
+                        Console.WriteLine("Line" + lineCount);
+                        var columns = line!.Split('\t');
+
+                        if (lineCount == 0 || lineCount > 5)
+                        {
+                            line = await stream.ReadLineAsync();
+                            lineCount++;
+                            continue;
+                        }
+
+                        if (columns.Length != 3)
+                        {
+                            throw new InvalidOperationException("The file is not a valid tab-separated file.");
+                        }
+
+                        if (DateTime.TryParse(columns[2], out var balanceDate) && decimal.TryParse(columns[1], out var balance))
+                        {
+                            if (!await isAccountBalancePresent(balanceDate))
+                            {
+
+                                var newAccountBalance = new AccountBalance
+                                {
+                                    AccountName = columns[0].ToString(),
+                                    Balance = balance,
+                                    BalanceDate = balanceDate
+                                };
+
+                                balances.Add(newAccountBalance);
+                            }
+                            else
+                            {
+
+                                throw new InvalidOperationException("Files contains account balance that is already present");
+                            }
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Invalid data found in file");
+                        }
+
+                        line = await stream.ReadLineAsync();
+                        lineCount++;
+                    }
+                }
+                break;
+            case FileType.xl:
+                {
+                    using var stream = new MemoryStream();
+                    await file.CopyToAsync(stream);
+                    using var package = new ExcelPackage(stream);
+                    var worksheet = package.Workbook.Worksheets.FirstOrDefault() ?? throw new InvalidOperationException("Data not found in file");
+
+                    var rowCount = worksheet.Dimension.Rows;
+                    if (worksheet.Dimension.Columns == 3)
+                    {
+                        for (int row = 2; row <= rowCount; row++)
+                        {
+                            if (DateTime.TryParse(worksheet.Cells[row, 3].Text, out var balanceDate) && decimal.TryParse(worksheet.Cells[row, 2].Text, out var balance))
+                            {
+                                if (!await isAccountBalancePresent(balanceDate))
+                                {
+                                    var newAccountBalance = new AccountBalance
+                                    {
+                                        AccountName = worksheet.Cells[row, 1].Text.ToString(),
+                                        Balance = balance,
+                                        BalanceDate = balanceDate
+                                    };
+
+                                    balances.Add(newAccountBalance);
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException("Files contains account balance that is already present");
+                                }
+                            }
+                            else
+                            {
+                                throw new InvalidOperationException("Invalid data found in file");
+                            }
+                        }
                     }
                     else
                     {
-                        throw new InvalidOperationException("Files contains account balance that is already present");
+                        throw new InvalidOperationException("The file is not a valid excel file.");
+
                     }
                 }
-
-                line = await stream.ReadLineAsync();
-                lineCount++;
-            }
+                break;
+            default: throw new InvalidOperationException("Unsuported file type");
         }
 
         return balances;
 
     }
 
-    private async Task HandleTabSeparatedFile(IFormFile file)
+    private async Task HandleFile(IFormFile file, FileType type)
     {
-        List<AccountBalance> balances = await ProcessTextFile(file);
+        List<AccountBalance> balances = await ProcessFile(file, type);
 
         foreach (AccountBalance balance in balances)
         {
             await _repo.CreateAsync(balance);
-        }
-    }
-
-    private async Task HandleExcelFile(IFormFile file)
-    {
-        using (var stream = new MemoryStream())
-        {
-            await file.CopyToAsync(stream);
-            using (var package = new ExcelPackage(stream))
-            {
-                var worksheet = package.Workbook.Worksheets.FirstOrDefault();
-                if (worksheet == null)
-                    return;
-
-                var rowCount = worksheet.Dimension.Rows;
-                for (int row = 2; row <= rowCount; row++)
-                {
-                    var accountName = worksheet.Cells[row, 1].Text;
-                    var balance = decimal.Parse(worksheet.Cells[row, 2].Text);
-                    var balanceDate = DateTime.Parse(worksheet.Cells[row, 3].Text);
-
-                    var accountBalance = new AccountBalance
-                    {
-                        AccountName = accountName,
-                        Balance = balance,
-                        BalanceDate = balanceDate
-                    };
-                    await _repo.CreateAsync(accountBalance);
-                }
-            }
         }
     }
 
@@ -133,24 +168,24 @@ public class BalancesController : ControllerBase
             return BadRequest("Data not found in file");
 
         var extension = Path.GetExtension(file.FileName);
-        if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase))
+        try
         {
-            try
+            if (extension.Equals(".txt", StringComparison.OrdinalIgnoreCase))
             {
-                await HandleTabSeparatedFile(file);
+                await HandleFile(file, FileType.tab);
             }
-            catch (Exception e)
+            else if (extension.Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
             {
-                return BadRequest(e.Message);
+                await HandleFile(file, FileType.xl);
+            }
+            else
+            {
+                return BadRequest("Unsupported file type");
             }
         }
-        else if (extension.Equals(".xlsx", StringComparison.OrdinalIgnoreCase))
+        catch (Exception e)
         {
-            await HandleExcelFile(file);
-        }
-        else
-        {
-            return BadRequest("Unsupported file type");
+            return BadRequest(e.Message);
         }
 
         return Ok();
